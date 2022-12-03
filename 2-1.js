4a3daad2518f81ec59b0cc8b90c1b6351282831d
StrangeQwarq <118792687+StrangeQwarq@users.noreply.github.com>
2022-12-03 00:30:01 -0500

    day 2

---
+++ 2-1.js
@@ -0,0 +1,14 @@
+var outcomeScores = {
+	A: {X: 1 + 3, Y: 2 + 6, Z: 3 + 0},
+	B: {X: 1 + 0, Y: 2 + 3, Z: 3 + 6},
+	C: {X: 1 + 6, Y: 2 + 0, Z: 3 + 3},
+};
+
+var scoreTotal = 0;
+
+document.getElementsByTagName('pre')[0].textContent.split("\n").filter(a => a.length == 3).forEach((round) => {
+	let choices = round.split(" ");
+	scoreTotal += outcomeScores[choices[0]][choices[1]];
+});
+
+console.log(scoreTotal);
