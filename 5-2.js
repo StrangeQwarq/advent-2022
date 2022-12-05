cc7189f5917ab19caf1e2e019a0361f75c74681e
StrangeQwarq <118792687+StrangeQwarq@users.noreply.github.com>
2022-12-05 02:23:29 -0500

    day 5

---
+++ 5-2.js
@@ -0,0 +1,24 @@
+var lines = document.getElementsByTagName('pre')[0].textContent.split("\n").filter(a => a.length > 0);
+var answer = "";
+var stacks = [[],[],[],[],[],[],[],[],[]];
+
+lines.slice(0, 8).reverse().forEach((line) => {
+	let column = 0;
+	line.match(/.{1,4}/g).map(x => x.replace(/[\s\[\]]/g, '')).forEach((v) => {
+		if (v)
+			stacks[column].push(v);
+		column++;
+	});
+});
+
+lines.slice(9).forEach((line) => {
+	let matches = line.match(/move (\d+) from (\d+) to (\d+)/).map(x => parseInt(x));
+	let fromStack = stacks[matches[2] - 1];
+	stacks[matches[3] - 1] = stacks[matches[3] - 1].concat(fromStack.splice(fromStack.length - matches[1]));
+});
+
+stacks.forEach((x) => {
+	answer += x[x.length - 1];
+});
+
+console.log(answer);
