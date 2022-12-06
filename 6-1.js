898b8fb5ff5f14a31fa7a2a0215b7722971f6d7e
StrangeQwarq <118792687+StrangeQwarq@users.noreply.github.com>
2022-12-06 00:25:42 -0500

    day 6

---
+++ 6-1.js
@@ -0,0 +1,13 @@
+var numUnique = 4;
+var data = document.getElementsByTagName('pre')[0].textContent;
+for (let i = 0; i < data.length; i++) {
+	let letters = {};
+	for (let j = 0; j < numUnique; j++) {
+		letters[data[i + j]] = true;
+	}
+
+	if (Object.keys(letters).length === numUnique) {
+		console.log(i + numUnique);
+		break;
+	}
+};
